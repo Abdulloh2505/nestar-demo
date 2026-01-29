ad8113fe888556c47bea822032f946f3049304ef
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-29 11:42:35 +0900

    feat: integration Header and Footer via Layouts

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,5 +1,14 @@
- const PropertyList = () => {
-    return <div>PROPERTY LIST</div>;
+import withLayouBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+ const PropertyList: NextPage = () => {
+   return (
+       <>
+         <Container>PROPERTY LIST</Container>
+
+         </>
+   );
  }
 
- export default PropertyList;
+ export default withLayouBasic(PropertyList);
